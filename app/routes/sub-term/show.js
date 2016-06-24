0e63b4945673336741336a6082e3fdde88ab5321
santuan <smrcouto88@gmail.com>
2016-06-24 19:06:24 -0300

    term list is sticky

--- app/routes/sub-term/show.js
+++ app/routes/sub-term/show.js
@@ -1,10 +1,9 @@
 import Ember from 'ember';
-z
+
 export default Ember.Route.extend({
 	model: function (params) {
       return Ember.RSVP.hash({
           term: this.store.find('term', {filter: {slug: params.term_slug}}).then(function (terms) { return terms.content.objectAt(0) }),
-          posts: this.get('store').find('post', {filter: {category_name: params.term_slug, tag: "subterm"}})
 	      posts: this.get('store').find('post', {filter: {category_name: params.term_slug}})
       });		
 	}
